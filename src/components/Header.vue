ad8cf8e1bbced0a8977a7c900f8ee6f0679992e0
ali1sba <hariri.ali.official@gmail.com>
2021-10-09 00:53:31 +0100

    fixing contact form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -283,40 +283,11 @@ ESI-SBA and COC'2021.
                           Send Message Us
                         </h5>
                       </div>
-                      <div>
-                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                          <div class="row">
-                            <div class="col-md-12 mb-3">
-                              <div class="form-group">
-                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                              </div>
-                            </div>
-                            <div class="col-md-12 mb-3">
-                              <div class="form-group">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                              </div>
-                            </div>
-                            <div class="col-md-12 mb-3">
-                              <div class="form-group">
-                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-                              </div>
-                            </div>
-                            <div class="col-md-12">
-                              <div class="form-group">
-                                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-                              </div>
-                            </div>
-                            <div class="col-md-12 text-center my-3">
-                              <div class="loading">Loading</div>
-                              <div class="error-message"></div>
-                              <div class="sent-message">Your message has been sent. Thank you!</div>
-                            </div>
-                            <div class="col-md-12 text-center">
-                              <button type="submit" class="button button-a button-big button-rouded">Send Message</button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
+                        <el-input v-model="formData.name" placeholder="Your name" style="margin: 10px 0px;"></el-input> 
+                        <el-input v-model="formData.email" placeholder="Your Email" style="margin: 10px 0px;"></el-input> 
+                        <el-input v-model="formData.subject" placeholder="Subject" style="margin: 10px 0px;"></el-input> 
+                        <el-input v-model="formData.message" :rows="5" type="textarea" placeholder="Message" style="margin: 10px 0px;"></el-input> 
+                        <el-button type="primary" round style="margin:10px 0px" @click="sendEmail"> Send the message </el-button>
                     </div>
                     <div class="col-md-6">
                       <div class="title-box-2 pt-4 pt-md-0">
@@ -395,15 +366,42 @@ ESI-SBA and COC'2021.
 </template>
 
 <script>
+import emailjs from 'emailjs-com';
 export default {
   data() {
     return {
+      formData:{
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+      },
       name: "",
       props: {
         msg: String,
       },
     };
   },
+  methods: {
+    sendEmail() {
+      try {
+        emailjs.send('service_dgukqy9', 'template_ziiud8f',this.formData,
+        'user_kiL2G409pIe4Z9n09Wkdw' );
+        this.$message({
+        title: 'Success',
+        message: 'message send :)',
+        type: 'success',
+      });
+
+      } catch(error) {
+          console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
+  }
 };
 </script>
 
